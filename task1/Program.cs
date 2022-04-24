528ddc328297fd42f822ab1569fc53000be7eece
vmaevski <vmaevski@mail.ru>
2022-04-24 20:12:19 +0700

    Completed task 1

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее.
+// Пример: a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+string s_b = Console.ReadLine();
+int b = int.Parse(s_b);
+
+if (a > b)
+{
+    Console.Write("Max = ");
+    Console.Write(a);
+}
+if (a < b)
+{ 
+    Console.Write("Max = ");
+    Console.Write(b);
+}
+if (a == b)
+{
+    Console.Write("Числа равны.");
+}
+
