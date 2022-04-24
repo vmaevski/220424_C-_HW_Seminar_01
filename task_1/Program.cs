82a08c049bb955646bf7a4447e4f72bf02b66176
vmaevski <vmaevski@mail.ru>
2022-04-24 21:25:16 +0700

    Completed task _1 from the seminar

---
+++ task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// . Напишите программу вычисления модуля числа.
+// 2 -> 2
+// -3 -> 3
+// -7 -> 7
+
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+if (a < 0)
+{
+    a = -a;
+}
+Console.WriteLine(a);
